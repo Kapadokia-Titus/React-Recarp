e99c7cd3987d5727501fc745671138ca34923629
kapadokia <ktnyandoch@gmail.com>
2023-05-21 12:33:10 +0300

    feat: able to read file data and update count

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
 import UploadForm from "./components/UploadForm";
 
-const photos =[
-  'https://picsum.photos/id/1001/200',
-  'https://picsum.photos/id/1002/200',
-  'https://picsum.photos/id/1003/200',
-  'https://picsum.photos/id/1004/200',
-  'https://picsum.photos/id/1005/200'
-]
+const photos = [
+  "https://picsum.photos/id/1001/200",
+  "https://picsum.photos/id/1002/200",
+  "https://picsum.photos/id/1003/200",
+  "https://picsum.photos/id/1004/200",
+  "https://picsum.photos/id/1005/200",
+];
 
 function App() {
-const [items, setItems] = useState(photos); 
-const [isCollapsed, collapse] = useState(false); 
+  const [count, setCount] = useState(); 
+  const [inputs, setInputs] = useState();
+  const [items, setItems] = useState(photos);
+  const [isCollapsed, collapse] = useState(false);
 
-const toggle = ()=> collapse(!isCollapsed);
+  const handleOnChange = (e) => {
+    if(e.target.name==='file'){
+ // method that saves files from a computer
+      setInputs({...inputs, file:e.target.files[0], path: URL.createObjectURL(e.target.files[0])})
+    }else {// method that saves files from a computer
+      setInputs({...inputs,title:e.target.value})
+    }
+   
+  };
+  const handleOnSubmit = (e) =>{
+    e.preventDefault()
+    setItems([inputs.path,...items])
+  } ;
+
+  useEffect(()=>{
+    setCount(`you have ${items.length} image${items.length > 1? "s":""}`)
+  },[items])
+
+  const toggle = () => collapse(!isCollapsed);
   return (
     <>
       <Navbar />
       <div class="container text-center mt-5">
-        <button className="btn btn-success float-end" onClick={toggle}>{isCollapsed? 'Hide Form':'+Add'}</button>
+        <button className="btn btn-success float-end" onClick={toggle}>
+          {isCollapsed ? "Hide Form" : "+Add"}
+        </button>
         <div className="clearfix mb-4"></div>
-        <UploadForm isVisible={isCollapsed}/>
+        <UploadForm
+          isVisible={isCollapsed}
+          handleOnChange={handleOnChange}
+          handleOnSubmit={handleOnSubmit}
+        />
         <h1>Gallery</h1>
-        <div className="row">
-             {items.map((photo)=><Card src={photo} />)} 
+        {count}
+        <div className="row mt-2">
+          {items?.map((photo) => (
+            <Card src={photo} />
+          ))}
         </div>
       </div>
     </>
--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,15 +1,16 @@
-export default function UploadForm({isVisible}) {
+export default function UploadForm({isVisible, handleOnChange, handleOnSubmit}) {
   return (
    isVisible && <>
     <p className="display-6 text-center mb-3">Upload Stock Image</p>
     <div className="mb-5 d-flex align-items-center justify-content-center">
-    <form>
+    <form className="mb-2" style={{textAlign:'left'}} onSubmit={handleOnSubmit}>
       <div className="form-group mb-3">
         <input
           type="text"
           className="form-control"
           name="title"
           placeholder="title"
+          onChange={handleOnChange}
           id="title"
           aria-describedby="text"
         />
@@ -18,8 +19,9 @@ export default function UploadForm({isVisible}) {
         <input
           type="file"
           className="form-control"
+          onChange={handleOnChange}
           name="file"
-        />
+        /> 
       </div>
       <button type="submit" className="btn btn-success float-end">
         Save Changes
