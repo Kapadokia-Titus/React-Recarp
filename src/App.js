a90e1ea0ddd37500a61ca145ed5e7dc2ec867b40
kapadokia <ktnyandoch@gmail.com>
2023-05-23 02:40:29 +0300

    successfully replaced useEffects with useContext hook

--- src/App.js
+++ src/App.js
@@ -1,61 +1,14 @@
-import { useEffect, useMemo, useReducer, useState } from "react";
+import { useContext, useMemo } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import Layout from "./components/Layout";
+import { Context } from "./context";
 
-const photos = [
-];
 
-// set initial state
-const initialState = {
-  items: photos,
-  count: photos.length,
-  inputs: { title: null, file: null, path: null },
-  isCollapsed: false,
-};
-
-// imputs handler
-const handleOnChange = (state, e) => {
-  if (e.target.name === "file") {
-    // method that saves files from a computer
-    return {
-      ...state.inputs,
-      file: e.target.files[0],
-      path: URL.createObjectURL(e.target.files[0]),
-    };
-  } else {
-    // method that saves files from a computer
-    return { ...state.inputs, title: e.target.value };
-  }
-};
-
-// reducer funciton
-function reducer(state, action) {
-  switch (action.type) {
-    case "setItem":
-      return {
-        ...state,
-        items: [state.inputs, ...state.items],
-        count: state.items.length +1 ,
-        inputs:{ title: null, file: null, path: null },
-      };
-    case "setInputs":
-      return {
-        ...state,
-        inputs: handleOnChange(state, action.payload.value),
-      };
-    case "collapse":
-      return {
-        ...state,
-        isCollapsed: action.payload.bool,
-      };
-    default:
-      return state;
-  }
-}
 function App() {
-  // use useReducer instead of useState, that r3eturns current state and dispatch
-  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // use the useContext hook to consume the stateful value
+  const {dispatch, state} = useContext(Context)
 
   const handleOnChange = (e) =>
     dispatch({ type: "setInputs", payload: { value: e } });
